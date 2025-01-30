43f08e3c7d1b8d49e071e892ee441e863e078251
Jordan Babb <normanwebdesigners@gmail.com>
2025-01-30 16:18:40 -0600

    updated title

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,19 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Pro Elevated Plumbin",
+    "email": "yourcompany@email.com",
+    "phoneForTel": "918-844-0084",
+    "phoneFormatted": "(918)  844-0084",
     "address": {
         "lineOne": "First Address Line",
         "lineTwo": "Second Address Line",
         "city": "Denver",
         "state": "CO",
         "zip": "80206",
-        "country": "US",
-        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+        "country" : "US",
+        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
-    "socials": {
-        "facebook": "https://www.facebook.com/",
-        "instagram": "https://www.instagram.com/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
-};
+    "domain": "https://www.codestitch.app"
+}
+
+
+
